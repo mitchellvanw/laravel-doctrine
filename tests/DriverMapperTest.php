cd8e01bb8cc543ebd12132aa2ad1eaf139f1c5ce
kirkbushell <torm3nt@gmail.com>
2014-09-12 22:05:14 +0100

    Got initial tests going

---
+++ tests/DriverMapperTest.php
@@ -0,0 +1,24 @@
+<?php namespace Tests;
+
+use Mitch\LaravelDoctrine\Configuration\DriverMapper;
+use Mockery as m;
+
+class DriverMapperTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUsageOfCorrectConfigurationMapper()
+	{
+		$mockMapper1 = m::mock('Mitch\LaravelDoctrine\Configuration\Mapper');
+		$mockMapper2 = m::mock('Mitch\LaravelDoctrine\Configuration\Mapper');
+
+		$driverMapper = new DriverMapper('some driver');
+
+		$driverMapper->registerMapper($mockMapper1);
+		$driverMapper->registerMapper($mockMapper2);
+
+		$mockMapper1->shouldReceive('appropriate')->once()->andReturn(false);
+		$mockMapper2->shouldReceive('appropriate')->once()->andReturn(true);
+		$mockMapper2->shouldReceive('map')->once()->andReturn('mapped array');
+
+		$this->assertEquals('mapped array', $driverMapper->map([]));
+	}
+}
