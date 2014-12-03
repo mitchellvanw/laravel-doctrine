6ea4a1560270d206a873dc766791c82c41fbded5
Mitchell van Wijngaarden <mitchell@kooding.nl>
2014-12-03 13:43:42 +0100

    Add PasswordReset trait

---
+++ src/Traits/PasswordReset.php
@@ -0,0 +1,14 @@
+<?php namespace Mitch\LaravelDoctrine\Traits;
+
+trait PasswordReset
+{
+    /**
+     * Get the e-mail address where password reset links are sent.
+     *
+     * @return string
+     */
+    public function getEmailForPasswordReset()
+    {
+        return $this->email;
+    }
+} 
