203933667935e0b3772e319e34d3c922547cd734
Darren Craig <darren@minus40.co>
2014-12-05 16:46:35 +0000

    Adding fixes for the latest L5 updates

--- src/DoctrineUserProvider.php
+++ src/DoctrineUserProvider.php
@@ -2,14 +2,14 @@
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
-use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\UserProviderInterface;
-use Illuminate\Hashing\HasherInterface;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Hashing\Hasher;
 
-class DoctrineUserProvider implements UserProviderInterface {
-
+class DoctrineUserProvider implements UserProviderInterface
+{
     /**
-     * @var HasherInterface
+     * @var Hasher
      */
     private $hasher;
     /**
@@ -22,32 +22,36 @@ class DoctrineUserProvider implements UserProviderInterface {
     private $entity;
 
     /**
-     * @param HasherInterface $hasher
+     * @param Hasher $hasher
      * @param EntityManager $entityManager
      * @param $entity
      */
-    public function __construct(HasherInterface $hasher, EntityManager $entityManager, $entity) {
+    public function __construct(Hasher $hasher, EntityManager $entityManager, $entity)
+    {
         $this->hasher = $hasher;
         $this->entityManager = $entityManager;
         $this->entity = $entity;
     }
-
     /**
      * Retrieve a user by their unique identifier.
+
      * @param  mixed $identifier
-     * @return UserInterface|null
+     * @return Authenticatable|null
      */
-    public function retrieveById($identifier) {
+    public function retrieveById($identifier)
+    {
         return $this->getRepository()->find($identifier);
     }
 
     /**
      * Retrieve a user by by their unique identifier and "remember me" token.
+
      * @param  mixed $identifier
      * @param  string $token
-     * @return UserInterface|null
+     * @return Authenticatable|null
      */
-    public function retrieveByToken($identifier, $token) {
+    public function retrieveByToken($identifier, $token)
+    {
         $entity = $this->getEntity();
         return $this->getRepository()->findOneBy([
             $entity->getKeyName() => $identifier,
@@ -57,11 +61,13 @@ class DoctrineUserProvider implements UserProviderInterface {
 
     /**
      * Update the "remember me" token for the given user in storage.
-     * @param  UserInterface $user
+
+     * @param  Authenticatable $user
      * @param  string $token
      * @return void
      */
-    public function updateRememberToken(UserInterface $user, $token) {
+    public function updateRememberToken(Authenticatable $user, $token)
+    {
         $user->setRememberToken($token);
         $this->entityManager->persist($user);
         $this->entityManager->flush();
@@ -69,40 +75,49 @@ class DoctrineUserProvider implements UserProviderInterface {
 
     /**
      * Retrieve a user by the given credentials.
+
      * @param  array $credentials
-     * @return UserInterface|null
+     * @return Authenticatable|null
      */
-    public function retrieveByCredentials(array $credentials) {
+    public function retrieveByCredentials(array $credentials)
+    {
         $criteria = [];
         foreach ($credentials as $key => $value)
             if ( ! str_contains($key, 'password'))
                 $criteria[$key] = $value;
+
         return $this->getRepository()->findOneBy($criteria);
     }
 
     /**
      * Validate a user against the given credentials.
-     * @param  UserInterface $user
+
+     * @param  Authenticatable $user
      * @param  array $credentials
      * @return bool
      */
-    public function validateCredentials(UserInterface $user, array $credentials) {
+    public function validateCredentials(Authenticatable $user, array $credentials)
+    {
         return $this->hasher->check($credentials['password'], $user->getAuthPassword());
     }
 
     /**
      * Returns repository for the entity.
+     *
      * @return EntityRepository
      */
-    private function getRepository() {
+    private function getRepository()
+    {
         return $this->entityManager->getRepository($this->entity);
     }
 
     /**
      * Returns instantiated entity.
+     *
      * @return mixed
      */
-    private function getEntity() {
+    private function getEntity()
+    {
         return new $this->entity;
     }
 }
