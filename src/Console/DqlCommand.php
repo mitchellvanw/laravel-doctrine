a6050c7f3acc2cae589b7f3bfcdc6ce482b77abb
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-10 00:53:55 +0100

    Add DQL console command

--- src/Console/DqlCommand.php
+++ src/Console/DqlCommand.php
@@ -7,32 +7,24 @@ use Symfony\Component\Console\Input\InputOption;
 
 class DqlCommand extends Command {
 
-    /**
-     * The console command name.
-     * @var string
-     */
     protected $name = 'doctrine:dql';
-
-    /**
-     * The console command description.
-     * @var string
-     */
     protected $description = 'Run a DQL query.';
 
-    /**
-     * The Entity Manager
-     * @var \Doctrine\ORM\EntityManagerInterface
-     */
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager) {
-        parent::__construct();
-
-        $this->entityManager = $entityManager;
-    }
-
     public function fire() {
-
+        $entityManager = $this->laravel->make('Doctrine\ORM\EntityManagerInterface');
+        $hydrationModeName = $this->option('hydrate');
+        $hydrationMode = 'Doctrine\ORM\Query::HYDRATE_' . strtoupper(str_replace('-', '_', $hydrationModeName));
+        if ( ! defined($hydrationMode))
+            throw new \RuntimeException("Hydration mode [{$hydrationModeName}] does not exist. It should be either: object. array, scalar, or single-scalar.");
+        $query = $entityManager->createQuery($this->argument('dql'));
+        $query->setFirstResult((int) $this->option('first-result'));
+        $query->setMaxResults((int) $this->option('max-result'));
+        if ($this->input->getOption('show-sql')) {
+            $this->line(Debug::dump($query->getSQL(), 2, true, false));
+            return;
+        }
+        $results = $query->execute(array(), constant($hydrationMode));
+        $this->output->writeln(Debug::dump($results, $this->option('depth'), true, false));
     }
 
     protected function getArguments() {
@@ -43,7 +35,11 @@ class DqlCommand extends Command {
 
     protected function getOptions() {
         return [
-            ['hydrate', null, InputOption::VALUE_OPTIONAL, 'Hydrate type. Available: object, array, scalar, single_scalar, simpleobject']
+            ['hydrate', null, InputOption::VALUE_REQUIRED, 'Hydrate type. Available: object, array, scalar, single_scalar, simpleobject.', 'object'],
+            ['first-result', null, InputOption::VALUE_REQUIRED, 'The first result in the result set.', 0],
+            ['max-result', null, InputOption::VALUE_REQUIRED, 'The maximum number of results in the result set.', 20],
+            ['depth', null, InputOption::VALUE_REQUIRED, 'Dumping depth of Entity graph.', 7],
+            ['sql', null, InputOption::VALUE_NONE, 'Dump generated SQL instead of executing query.'],
         ];
     }
 } 
