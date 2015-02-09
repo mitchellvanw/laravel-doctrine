584fa74b8ab52d07f6b97bc09fec5d14ac323b25
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-10 00:11:04 +0100

    Comply to new code style

--- src/Validation/DoctrinePresenceVerifier.php
+++ src/Validation/DoctrinePresenceVerifier.php
@@ -6,88 +6,75 @@ use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Validation\PresenceVerifierInterface;
 
-class DoctrinePresenceVerifier implements PresenceVerifierInterface
-{
-	protected $entityManager;
+class DoctrinePresenceVerifier implements PresenceVerifierInterface {
 
-	public function __construct(EntityManagerInterface $entityManager)
-	{
-		$this->entityManager = $entityManager;
-	}
-	/**
-	 * Count the number of objects in a collection having the given value.
-	 *
-	 * @param  string $collection
-	 * @param  string $column
-	 * @param  string $value
-	 * @param  int $excludeId
-	 * @param  string $idColumn
-	 * @param  array $extra
-	 * @return int
-	 */
-	public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = array())
-	{
-		$queryParts = ['SELECT COUNT(*) FROM', $collection, 'WHERE', "$column = ?"];
+    protected $entityManager;
 
-		if (!is_null($excludeId) && $excludeId != 'NULL')  {
-			$queryParts[] = 'AND '.($idColumn ?: 'id').' <> ?';
-		}
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
 
-		foreach ($extra as $key => $extraValue) {
-			$queryParts[] = "AND $key = ?";
-		}
+    /**
+     * Count the number of objects in a collection having the given value.
+     * @param  string $collection
+     * @param  string $column
+     * @param  string $value
+     * @param  int $excludeId
+     * @param  string $idColumn
+     * @param  array $extra
+     * @return int
+     */
+    public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = []) {
+        $queryParts = ['SELECT COUNT(*) FROM', $collection, 'WHERE', "$column = ?"];
 
-		$query = $this->createQueryFrom($queryParts);
-		$query->setParameter(1, $value);
+        if ( ! is_null($excludeId) && $excludeId != 'NULL')
+            $queryParts[] = 'AND ' . ($idColumn ?: 'id') . ' <> ?';
 
-		if (!is_null($excludeId) && $excludeId != 'NULL')  {
-			$query->setParameter(2, $excludeId);
-		}
+        foreach ($extra as $key => $extraValue)
+            $queryParts[] = "AND $key = ?";
 
-		foreach ($extra as $key => $extraValue) {
-			$query->setParameter($key + 3, $extraValue);
-		}
+        $query = $this->createQueryFrom($queryParts);
+        $query->setParameter(1, $value);
 
-		return $query->getSingleScalarResult();
-	}
+        if ( ! is_null($excludeId) && $excludeId != 'NULL')
+            $query->setParameter(2, $excludeId);
 
-	/**
-	 * Count the number of objects in a collection with the given values.
-	 *
-	 * @param  string $collection
-	 * @param  string $column
-	 * @param  array $values
-	 * @param  array $extra
-	 * @return int
-	 */
-	public function getMultiCount($collection, $column, array $values, array $extra = array())
-	{
-		$queryParts = ['SELECT COUNT(*) FROM', $collection, 'WHERE', "$column IN (?)"];
+        foreach ($extra as $key => $extraValue)
+            $query->setParameter($key + 3, $extraValue);
 
-		foreach ($extra as $key => $extraValue) {
-			$queryParts[] = "AND $key = ?";
-		}
+        return $query->getSingleScalarResult();
+    }
 
-		$query = $this->createQueryFrom($queryParts);
-		$query->setParameter(1, implode(',', $values));
+    /**
+     * Count the number of objects in a collection with the given values.
+     * @param  string $collection
+     * @param  string $column
+     * @param  array $values
+     * @param  array $extra
+     * @return int
+     */
+    public function getMultiCount($collection, $column, array $values, array $extra = []) {
+        $queryParts = ['SELECT COUNT(*) FROM', $collection, 'WHERE', "$column IN (?)"];
 
-		foreach ($extra as $key => $extraValue) {
-			$query->setParameter($key + 2, $extraValue);
-		}
+        foreach ($extra as $key => $extraValue)
+            $queryParts[] = "AND $key = ?";
 
-		return $query->count();
-	}
+        $query = $this->createQueryFrom($queryParts);
+        $query->setParameter(1, implode(',', $values));
 
-	/**
-	 * Creates a new Doctrine native query based on the query parts array.
-	 *
-	 * @param array $queryParts
-	 * @return mixed
-	 */
-	private function createQueryFrom(array $queryParts = [])
-	{
-		$rsm = new ResultSetMapping();
+        foreach ($extra as $key => $extraValue)
+            $query->setParameter($key + 2, $extraValue);
 
-		return $this->entityManager->createNativeQuery(implode(' ', $queryParts), $rsm);
-	}
+        return $query->count();
+    }
+
+    /**
+     * Creates a new Doctrine native query based on the query parts array.
+     * @param array $queryParts
+     * @return mixed
+     */
+    private function createQueryFrom(array $queryParts = []) {
+        $rsm = new ResultSetMapping;
+        return $this->entityManager->createNativeQuery(implode(' ', $queryParts), $rsm);
+    }
 }
