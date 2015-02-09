49657094eaf153aa9641f65089f2e804762b8062
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-10 00:04:28 +0100

    Search recursive in entity and fix Soft Deletable bug. Close #83

--- src/EventListeners/SoftDeletableListener.php
+++ src/EventListeners/SoftDeletableListener.php
@@ -3,23 +3,20 @@
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use DateTime;
 
-class SoftDeletableListener
-{
-    public function onFlush(OnFlushEventArgs $event)
-    {
+class SoftDeletableListener {
+
+    public function onFlush(OnFlushEventArgs $event) {
         $entityManager = $event->getEntityManager();
         $unitOfWork = $entityManager->getUnitOfWork();
         foreach ($unitOfWork->getScheduledEntityDeletions() as $entity) {
             if ($this->isSoftDeletable($entity)) {
                 $metadata = $entityManager->getClassMetadata(get_class($entity));
                 $oldDeletedAt = $metadata->getFieldValue($entity, 'deletedAt');
-                if ($oldDeletedAt instanceof DateTime) {
+                if ($oldDeletedAt instanceof DateTime)
                     continue;
-                }
                 $now = new DateTime;
                 $metadata->setFieldValue($entity, 'deletedAt', $now);
                 $entityManager->persist($entity);
-
                 $unitOfWork->propertyChanged($entity, 'deletedAt', $oldDeletedAt, $now);
                 $unitOfWork->scheduleExtraUpdate($entity, [
                     'deletedAt' => [$oldDeletedAt, $now]
@@ -28,8 +25,7 @@ class SoftDeletableListener
         }
     }
 
-    private function isSoftDeletable($entity)
-    {
-        return array_key_exists('Mitch\LaravelDoctrine\Traits\SoftDeletes', class_uses($entity));
+    private function isSoftDeletable($entity) {
+        return array_key_exists('Mitch\LaravelDoctrine\Traits\SoftDeletes', class_uses_recursive(get_class($entity)));
     }
 }
