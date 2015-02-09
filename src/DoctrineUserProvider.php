bb88ece48e9a830d6a74aefea58ef6d98fda4048
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-10 00:26:15 +0100

    Change last files to new code style

--- src/DoctrineUserProvider.php
+++ src/DoctrineUserProvider.php
@@ -6,8 +6,8 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\UserProviderInterface;
 use Illuminate\Hashing\HasherInterface;
 
-class DoctrineUserProvider implements UserProviderInterface
-{
+class DoctrineUserProvider implements UserProviderInterface {
+
     /**
      * @var HasherInterface
      */
@@ -26,32 +26,28 @@ class DoctrineUserProvider implements UserProviderInterface
      * @param EntityManager $entityManager
      * @param $entity
      */
-    public function __construct(HasherInterface $hasher, EntityManager $entityManager, $entity)
-    {
+    public function __construct(HasherInterface $hasher, EntityManager $entityManager, $entity) {
         $this->hasher = $hasher;
         $this->entityManager = $entityManager;
         $this->entity = $entity;
     }
+
     /**
      * Retrieve a user by their unique identifier.
-
      * @param  mixed $identifier
      * @return UserInterface|null
      */
-    public function retrieveById($identifier)
-    {
+    public function retrieveById($identifier) {
         return $this->getRepository()->find($identifier);
     }
 
     /**
      * Retrieve a user by by their unique identifier and "remember me" token.
-
      * @param  mixed $identifier
      * @param  string $token
      * @return UserInterface|null
      */
-    public function retrieveByToken($identifier, $token)
-    {
+    public function retrieveByToken($identifier, $token) {
         $entity = $this->getEntity();
         return $this->getRepository()->findOneBy([
             $entity->getKeyName() => $identifier,
@@ -61,13 +57,11 @@ class DoctrineUserProvider implements UserProviderInterface
 
     /**
      * Update the "remember me" token for the given user in storage.
-
      * @param  UserInterface $user
      * @param  string $token
      * @return void
      */
-    public function updateRememberToken(UserInterface $user, $token)
-    {
+    public function updateRememberToken(UserInterface $user, $token) {
         $user->setRememberToken($token);
         $this->entityManager->persist($user);
         $this->entityManager->flush();
@@ -75,49 +69,40 @@ class DoctrineUserProvider implements UserProviderInterface
 
     /**
      * Retrieve a user by the given credentials.
-
      * @param  array $credentials
      * @return UserInterface|null
      */
-    public function retrieveByCredentials(array $credentials)
-    {
+    public function retrieveByCredentials(array $credentials) {
         $criteria = [];
         foreach ($credentials as $key => $value)
             if ( ! str_contains($key, 'password'))
                 $criteria[$key] = $value;
-
         return $this->getRepository()->findOneBy($criteria);
     }
 
     /**
      * Validate a user against the given credentials.
-
      * @param  UserInterface $user
      * @param  array $credentials
      * @return bool
      */
-    public function validateCredentials(UserInterface $user, array $credentials)
-    {
+    public function validateCredentials(UserInterface $user, array $credentials) {
         return $this->hasher->check($credentials['password'], $user->getAuthPassword());
     }
 
     /**
      * Returns repository for the entity.
-     *
      * @return EntityRepository
      */
-    private function getRepository()
-    {
+    private function getRepository() {
         return $this->entityManager->getRepository($this->entity);
     }
 
     /**
      * Returns instantiated entity.
-     *
      * @return mixed
      */
-    private function getEntity()
-    {
+    private function getEntity() {
         return new $this->entity;
     }
 }
--- src/EntityManagerFacade.php
+++ src/EntityManagerFacade.php
@@ -1,16 +1,14 @@
-<?php namespace Mitch\LaravelDoctrine; 
+<?php namespace Mitch\LaravelDoctrine;
 
 use Illuminate\Support\Facades\Facade;
 
-class EntityManagerFacade extends Facade
-{
+class EntityManagerFacade extends Facade {
+
     /**
      * Get the registered name of the component.
-     *
      * @return string
      */
-    protected static function getFacadeAccessor()
-    {
+    protected static function getFacadeAccessor() {
         return 'Doctrine\ORM\EntityManager';
     }
 } 
