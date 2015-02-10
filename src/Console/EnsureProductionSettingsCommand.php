48afbf80188777b2ca4980d9acdf5e18c728bba4
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-10 01:01:36 +0100

    Add Ensure Production Settings command

---
+++ src/Console/EnsureProductionSettingsCommand.php
@@ -0,0 +1,29 @@
+<?php namespace Mitch\LaravelDoctrine\Console;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class EnsureProductionSettingsCommand extends Command {
+
+    protected $name = 'doctrine:ensure-production-settings';
+    protected $description = 'Verify that Doctrine is properly configured for a production environment.';
+
+    public function fire() {
+        $entityManager = $this->laravel->make('Doctrine\ORM\EntityManagerInterface');
+        try {
+            $entityManager->getConfiguration()->ensureProductionSettings();
+            if ($this->option('complete'))
+                $entityManager->getConnection()->connect();
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+            return 1;
+        }
+        $this->info('Environment is correctly configured for production.');
+    }
+
+    protected function getOptions() {
+        return [
+            ['complete', null, InputOption::VALUE_NONE, 'Also inspect database connection existence.'],
+        ];
+    }
+} 
