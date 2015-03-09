ff30baa18f227f543a1d11d3f9ddb74cb0974cfa
Jason Funk <jasonlfunk@gmail.com>
2015-03-09 13:54:44 -0500

    Match style

--- src/EventListeners/SoftDeletableListener.php
+++ src/EventListeners/SoftDeletableListener.php
@@ -12,9 +12,8 @@ class SoftDeletableListener {
             if ($this->isSoftDeletable($entity)) {
                 $metadata = $entityManager->getClassMetadata(get_class($entity));
                 $oldDeletedAt = $metadata->getFieldValue($entity, 'deletedAt');
-                if ($oldDeletedAt instanceof DateTime) {
+                if ($oldDeletedAt instanceof DateTime)
                     continue;
-                }
                 $now = new DateTime;
                 $metadata->setFieldValue($entity, 'deletedAt', $now);
                 $entityManager->persist($entity);
